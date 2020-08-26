319c3edf5a50783daaa02f5363455ff20a5b4f1d
hello-liwan <58982538+hello-liwan@users.noreply.github.com>
2020-08-27 04:48:47 +0800

    斐波那契数列

---
+++ 斐波那契数列test.cpp
@@ -0,0 +1,67 @@
+﻿/*斐波那契数列dp*/
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int num[100000];
+int main()
+{
+	num[0] = 0;
+	num[1] = 1;
+	int n;
+	cin >> n;
+	for (int i = 2; i < n; i++) {
+		num[i] = num[i - 2] + num[i - 1];
+	}
+	cout << num[n - 1];
+	return 0;
+}
+
+
+
+/*斐波那契数列普通递归*/
+
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int fac(int num) {
+	if (num == 1||num == 2) {
+		return 1;
+	}
+	if (num > 2) {
+		return fac(num - 1) + fac(num - 2);
+	}
+	return 0;
+}
+int main()
+{
+	int n;
+	cin >> n;
+	cout << fac(n);
+	return 0;
+};
+
+
+
+/*斐波那契数列记忆优化递归*/
+#include<iostream>
+#include<algorithm>
+using namespace std;
+int a[10000];
+int fac(int num) {
+	if (num == 1 || num == 2) {
+		a[num] = 1;
+		return 1;
+	}
+	if (a[num]) {
+		return a[num];
+	}
+	a[num] = fac(num - 1) + fac(num - 2);
+	return a[num];
+}
+int main() 
+{
+	int n;
+	cin >> n;
+	cout << fac(n);
+}
